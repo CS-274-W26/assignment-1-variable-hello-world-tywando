520d2a9b8b4e877282dbf7110421c30ce786f340
Tyler Ebner <ebnerty@flip1.engr.oregonstate.edu>
2026-01-19 10:21:17 -0800

    Completed Assignment

--- prog1.c
+++ prog1.c
@@ -1,11 +1,20 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <time.h> // Provides time() for seeding
+#include <stdlib.h> // Provides rand() and srand()
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+	srand(time(NULL)); // Seed Pseudo-Random Number Generation (PRNG)
+
+	int myrand; // Declare int myrand
+	myrand = rand() % 11; // Intitialize myrand to between 0 and 10
+
+	if (myrand >= 0 && myrand <= 4) {
+		printf("Eat more beef, kick less cats\n");
+	} else if (myrand >= 5 && myrand <= 9) {
+		printf("FRODO LIVES\n");
+	} else {
+		printf("Larn is the best roguelike\n");
+	}
+
+	printf("The random number was: %d\n", myrand);
 }
